a5e440db2b02d08e10488adf67a10098e7e981e1
Gabriela <gabrielaquilarque97@gmail.com>
2017-05-10 20:12:03 +0000

    [ADD] Add new files to init development the new module about Partner Credit Limit and Overdue Payments.

---
+++ partner_credit_limit/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+############################################################################
+#    Module Writen For Odoo, Open Source Management Solution
+#
+#    coded by: Gabriela Quilarque, <gabrielaquilarque97@gmail.com>
+############################################################################
+from . import models
---
+++ partner_credit_limit/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+############################################################################
+#    Module Writen For Odoo, Open Source Management Solution
+#
+#    coded by: Gabriela Quilarque, <gabrielaquilarque97@gmail.com>
+############################################################################
+{
+    'name': 'Partner Credit Limit',
+    'version': '10.0.0.1.0',
+    'category': '',
+    'sequence': 15,
+    'summary': 'Credit Limit and Overdue Payments',
+    'website': '',
+    'depends': ['sales'],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'css': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
