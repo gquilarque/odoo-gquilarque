7f5a33a4d0c9b2b35bbf086b331d1cf43f9ff9e2
Gabriela <gabrielaquilarque97@gmail.com>
2017-05-21 08:53:34 +0000

    [IMP] Added new One2many field to show only the payments that the partner has overdue from sale.order

--- partner_credit_overdue/models/sale.py
+++ partner_credit_overdue/models/sale.py
@@ -6,7 +6,7 @@
 ############################################################################
 
 import odoo.addons.decimal_precision as dp
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class SaleOrder(models.Model):
@@ -23,3 +23,21 @@ class SaleOrder(models.Model):
     credit_requested = fields.Monetary(
         related='amount_total',
         readonly=True)
+    payment_overdue_ids = fields.One2many(
+        comodel_name='account.move.line',
+        inverse_name='move_id',
+        string='Payment Overdue',
+        compute="_compute_payment_overdue")
+
+    @api.multi
+    def _compute_payment_overdue(self):
+        moveline_obj = self.env['account.move.line']
+        for sale in self:
+            movelines = moveline_obj.search(
+                [('partner_id', '=', sale.partner_id.id),
+                 ('account_id.user_type_id.type', '=', 'receivable'),
+                 ('debit', '!=', 0),
+                 ('move_id.state', '!=', 'draft'),
+                 ('reconciled', '=', False),
+                 ('date_maturity', '<=', fields.Date.today())])
+            sale.payment_overdue_ids = movelines
